2d9a73d1145c5386eb263cca19452078ea161d72
Tatianemun <160507980+Tatianemun@users.noreply.github.com>
2024-04-09 12:20:45 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <h1>Agenda de contatos</h1>
+    <?php
+    //teste do php
+    echo"PHP funcionando!"."<br>";
+
+    //inclui um arquivo php
+    include 'conecta.php';
+
+    
+    //consulta ao banco de dados
+
+    $sql = "SELECT * FROM agenda";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Nome: " . $row["nome"]. " " - Telefone: " . $row["telefone"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+
+    ?>
+    <img src="agendadecontatos.png" alt="imagem de uma agenda de contatos">
+    <button type="button">Consultar Agenda</button>
+    <p>ID: NOME: TELEFONE:</p>
+    <p id="agenda">...</p>
+
+    <p>produzido por: Tatiane Muniz</p>
+</body>
+</html>
